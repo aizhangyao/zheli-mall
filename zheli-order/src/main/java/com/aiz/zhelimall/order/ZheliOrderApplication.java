8c2370cb1bccee7db23c896b45b3b4bafe0eefc3
aizhangyao <zhangyao9707@163.com>
2020-06-13 21:30:35 +0800

    feat(zheli-mall):use spring cloud alibaba cacos to enable discovery client

--- zheli-order/src/main/java/com/aiz/zhelimall/order/ZheliOrderApplication.java
+++ zheli-order/src/main/java/com/aiz/zhelimall/order/ZheliOrderApplication.java
@@ -3,6 +3,7 @@ package com.aiz.zhelimall.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 一.整合Mybatis-Plus
@@ -21,6 +22,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *          (2).高速MyBatis-Plus，sql映射文件位置
  *
  */
+@EnableDiscoveryClient
 @MapperScan("com.aiz.zhelimall.order.dao")
 @SpringBootApplication
 public class ZheliOrderApplication {
