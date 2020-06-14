fedab36f4c283a351f1c6f304ede0a5031a644d8
aizhangyao <zhangyao9707@163.com>
2020-06-14 11:11:37 +0800

    feat(zheli-mall):use openfeign

--- zheli-member/src/main/java/com/aiz/zhelimall/member/ZheliMemberApplication.java
+++ zheli-member/src/main/java/com/aiz/zhelimall/member/ZheliMemberApplication.java
@@ -3,7 +3,16 @@ package com.aiz.zhelimall.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * 1.想要远程调用别的服务
+ *  1).引入open-feign依赖
+ *  2).编写一个接口，告诉SpringCloud这个接口需要远程调用远程服务
+ *     (1).在接口中每一个方法都是调用哪个远程服务的哪个请求
+ *  3).开启远程调用功能
+ */
+@EnableFeignClients(basePackages = "com.aiz.zhelimall.member.feign")
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ZheliMemberApplication {
