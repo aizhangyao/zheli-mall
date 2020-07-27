8bc62e9263c20396b216618feac47dadbfda8dcd
aizhangyao <zhangyao9707@163.com>
2020-07-27 21:29:13 +0800

    add search module

---
+++ zheli-search/src/main/java/com/aiz/zhelimall/search/config/ZheliElasticSearchConfig.java
@@ -0,0 +1,35 @@
+package com.aiz.zhelimall.search.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 1.导入依赖
+ * 2.编写配置，给容器中注入一个RestHighLevelClient
+ * 3.参考API https://www.elastic.co/guide/en/elasticsearch/client/java-rest/7.x/java-rest-high.html
+ */
+@Configuration
+public class ZheliElasticSearchConfig {
+
+    public static final RequestOptions COMMON_OPTIONS;
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+//        builder.addHeader("Authorization", "Bearer " + TOKEN);
+//        builder.setHttpAsyncResponseConsumerFactory(
+//                new HttpAsyncResponseConsumerFactory
+//                        .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));
+        COMMON_OPTIONS = builder.build();
+    }
+
+    @Bean
+    public RestHighLevelClient esRestClient() {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("119.23.25.235", 9200, "http")));
+        return client;
+    }
+}
